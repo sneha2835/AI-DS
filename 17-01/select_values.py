3a029d1f5e906dc6af562ecb66dbd5fcf05aeae4
sneha2835 <snehakamatam28@gmail.com>
2025-01-17 14:12:20 +0530

    MySQL DataBase Connection Commands

--- 17-01/select_values.py
+++ 17-01/select_values.py
@@ -1,12 +1,14 @@
 import mysql.connector as c
+
 mydb = c.connect(
     host="localhost",
     user="root",
     password="system",
     database="mydatabase"
 )
-mycursor = mydb.cursor()
-mycursor.execute(select *from classes order by desc;)
-mycursor.execute(select *from classes where branch='ds';)
-print("yes")
+mycursor=mydb.cursor()
+mycursor.execute("select * from classes ")
+students=mycursor.fetchall()
+for std in students:
+    print(std)
 mydb.commit()
